f4effa5015e58c14315df4aec8aa34ac2d67b4fe
Robbie Leslie <143364684+leslier7@users.noreply.github.com>
2024-08-19 23:52:04 -0400

    Inital prototype of bluetooth server

--- bluetooth_server/src/main.cpp
+++ bluetooth_server/src/main.cpp
@@ -11,6 +11,9 @@
 #define SSID_UUID "ab85696b-0f23-4274-8f5f-886640962c0a"
 #define PASSWORD_UUID "05a4ce96-6214-4216-9986-ba51a47c0853"
 #define CLIENTKEY_UUID "592e1d83-70a4-4c3e-8917-dcd2c63985fb"
+//#define AUTH_UUID "4b1dcf29-6c1d-498b-844b-4169bace5de0"
+#define SECRET_UUID "27ea72f0-6f5a-4274-9220-80dcb1778570"
+
 
 class WiFiCredentials {
 public:
@@ -46,6 +49,9 @@ public:
     void onWrite(BLECharacteristic *pCharacteristic) override {
         String value = String(pCharacteristic->getValue().c_str());
 
+        Serial.print("onWrite called for UUID: ");
+        Serial.println(uuid);
+
         if (value.length() > 0) {
             Serial.print("New value for ");
             Serial.print(uuid);
@@ -61,6 +67,8 @@ public:
             } else if (uuid == CLIENTKEY_UUID) {
                 credentials->key = value;
                 Serial.println("Client Key: " + credentials->key);
+            } else if (uuid == SECRET_UUID) {
+                Serial.println("Secret: " + value);
             }
         }
     }
@@ -73,7 +81,7 @@ class MyServerCallbacks : public BLEServerCallbacks {
 
     void onDisconnect(BLEServer *pServer) {
         Serial.println("Client disconnected - restarting advertising");
-        BLEAdvertising *pAdvertising = pServer->getAdvertising();
+        BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
         pAdvertising->start();
     }
 };
@@ -99,28 +107,60 @@ void setup() {
 
     pCharacteristic->setValue("Hello World");
 
+    BLEDescriptor *pDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
+    pDescriptor->setValue("A characteristic to read and write");
+    pCharacteristic->addDescriptor(pDescriptor);
+
+
+    //Setting up the characteristic for the SSID
     BLECharacteristic *ssidCharacteristic = pService->createCharacteristic(
             SSID_UUID,
             BLECharacteristic::PROPERTY_WRITE
     );
     ssidCharacteristic->setCallbacks(new WiFiCallbacks(&credentials, SSID_UUID));
 
+    //Descriptor for the SSID characteristic (so it says "SSID" in an app)
+    BLEDescriptor *ssidDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
+    ssidDescriptor->setValue("SSID");
+    ssidCharacteristic->addDescriptor(ssidDescriptor);
+    ssidCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+
+    //Setting up the characteristic for the password
     BLECharacteristic *passwordCharacteristic = pService->createCharacteristic(
             PASSWORD_UUID,
             BLECharacteristic::PROPERTY_WRITE
     );
     passwordCharacteristic->setCallbacks(new WiFiCallbacks(&credentials, PASSWORD_UUID));
 
+    //Descriptor for the password characteristic (so it says "Password" in an app)
+    BLEDescriptor *passwordDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
+    passwordDescriptor->setValue("Password");
+    passwordCharacteristic->addDescriptor(passwordDescriptor);
+    passwordCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+
+    //Setting up the characteristic for the client key
     BLECharacteristic *keyCharacteristic = pService->createCharacteristic(
             CLIENTKEY_UUID,
             BLECharacteristic::PROPERTY_WRITE
     );
     keyCharacteristic->setCallbacks(new WiFiCallbacks(&credentials, CLIENTKEY_UUID));
 
-    pService->start();
+    //Descriptor for the key characteristic (so it says "Client Key" in an app)
+    BLEDescriptor *keyDescriptor = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
+    keyDescriptor->setValue("Client Key");
+    keyCharacteristic->addDescriptor(keyDescriptor);
+    keyCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
 
-    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    //Starting the server and advertising it
+    pService->start();
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
     pAdvertising->start();
+
+    //Setting up the security
+    //BLESecurity *pSecurity = new BLESecurity();
+    //pSecurity->setStaticPIN(123456);
+
+
 }
 
 void loop() {
